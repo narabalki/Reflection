671b1225e7645386bda383a455f4df5ac982188f
narabalki <narayananb@avaz.in>
2017-11-19 20:40:18 +0530

    Display DR dynamically, DR cache added

---
+++ Assets/Scripts/DR/DRCache.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DRCache : MonoBehaviour
+{
+	public static List<string> DRLanguages = new List<string> { "EN" };
+	public static DRCache instance;
+	public Dictionary<string, List<DailyReflection>> drList;
+	public Dictionary<string, Dictionary<string, DailyReflection>> drMap; 
+	private int recentDays = 1;
+
+	public void InitDRMap () {
+		drMap = new Dictionary<string, Dictionary<string, DailyReflection>> ();
+		drList = new Dictionary<string, List<DailyReflection>> ();
+		foreach (string lang in DRLanguages) {
+			drMap [lang] = new Dictionary<string, DailyReflection> ();
+			drList [lang] = new List<DailyReflection> ();
+		}
+	}
+
+	public void OnReceivedDR (DailyReflection dr) {
+		if (String.IsNullOrEmpty (dr.error)) {
+			drMap [dr.language] [dr.date] = dr;
+			drList [dr.language].Add (dr);
+		}
+	}
+
+	private void CacheRecentDRs () {
+		foreach (string lang in DRLanguages) {
+			for (int i = 0; i < recentDays; i++) {
+				StartCoroutine(DailyReflection.FetchDR (DateTime.Today.ToString ("yyyy-MM-dd"), lang, OnReceivedDR));
+			}
+		}
+	}
+
+	void MakeSingleton () {
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		} else {
+			Destroy (this);
+		}
+	}
+
+	void Awake () {
+		MakeSingleton ();
+	}
+
+	void Start () {
+		InitDRMap ();
+		CacheRecentDRs ();
+	}
+
+	public DailyReflection GetLatestDR(string lang="EN") {
+		DailyReflection dr = null;
+		if (drList.ContainsKey (lang)) {
+			if (drList [lang].Count > 0)
+				dr = drList [lang] [0];
+		}
+		return dr;
+	}
+
+}
+
--- Assets/Scripts/DR/DailyReflection.cs
+++ Assets/Scripts/DR/DailyReflection.cs
@@ -1,40 +1,108 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
 
-public class DailyReflection : MonoBehaviour {
-	public static string dr_url = "http://dr.sahajmarg.org/sm_view_dr/show_dr?lang=EN&display_dt=2002-01-01";
-	// Use this for initialization
-	void Start () {
+public class DailyReflection
+{
+	public string title;
+	public string message;
+	public string date;
+	public string author;
+	public string language;
+	public string html;
+	public string error;
+
+	#if false
+	private static string dr_url = "http://dr.sahajmarg.org/sm_view_dr/show_dr?lang=EN&display_dt=2002-01-01";
+	private static string title_tag = "<td width='48%' height='32' align='left' valign='middle'><p><span style='font-size: 20pt; color: #496072; font-weight: bold; font-family:arial, sans-serif'>";
+	private static string title_end_tag = "</span>";
+	private static string date_tag = "<span style='text-decoration: none; color: #272727; font-size: 14px; color: #46586C; font-weight: bold; font-family:arial, sans-serif'>";
+	private static string date_end_tag = "</span>";
+	private static string body_tag = "<td colspan='2' align='left' valign='top' style='font:Arial, Helvetica, sans-serif; font-size:16pt; padding-top:5px;color: #000;'>";
+	private static string body_end_tag = "</td>";
+	private static string author_tag = "<em><i>";
+	private static string author_end_tag = "</i>";
+	#else
+	private static string title_tag = "<title>";
+	private static string title_end_tag = "</title>";
+	private static string date_tag = "<display-dt type=\"date\">";
+	private static string date_end_tag = "</display-dt>";
+	private static string body_tag = "<content>";
+	private static string body_end_tag = "</content>";
+	private static string author_tag = "<footnote>";
+	private static string author_end_tag = "</footnote>";
+	private static string dr_web_service = "http://dr.sahajmarg.org/sm_view_dr/show_xml";
+	#endif
+
+	private static string GetBetween(string strSource, string strStart, string strEnd) {
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		int start, end;
+		string substring = "";
+		if (strSource.Contains (strStart) && strSource.Contains (strEnd)) {
+			start = strSource.IndexOf (strStart, 0) + strStart.Length;
+			end = strSource.IndexOf (strEnd, start);
+			substring = strSource.Substring (start, end - start);
+		}
+		return substring;
 	}
 
-	public static IEnumerator FetchDR() {
-		Dictionary<string, string> headerInfo = new Dictionary<string, string>();
-		WWW download = new WWW (dr_url, null, headerInfo);
-		yield return download;
-		if(!string.IsNullOrEmpty(download.error)) {
-			print( "Error downloading: " + download.error );
+	public DailyReflection(string lang, string fetchDate, string _html, string _error) {
+		
+		language = lang;
+		date = fetchDate;
+		html = _html;
+		title = message = author = error = "";
+		if (!string.IsNullOrEmpty (_error)) {
+			Debug.Log ("Error downloading DR for fetch date: " + fetchDate + " with error: " + _error);
+			error = _error;
 		} else {
-			// show the highscores
-			Debug.Log(download.text);
-			#if false
-			XElement root = XElement.Parse (download.text);
-			var nodes = root.Descendants("meta")
-				.Select(y => y.Descendants()
-					.Where(x => x.Attributes["property"].Value == "og:title"))
-				.ToList();
-			string title = string.Join (" ", nodes.ToArray ());
-			Debug.Log(title);
-			#endif
+			title = GetBetween (html, title_tag, title_end_tag).Trim ();
+			message = GetBetween (html, body_tag, body_end_tag).Trim ();
+			date = GetBetween (html, date_tag, date_end_tag).Trim ();
+			author = GetBetween (html, author_tag, author_end_tag).Trim ();
 		}
+	}
+
+	private void DebugLogDR() {
+		
+		Debug.Log (language);
+		Debug.Log (title);
+		Debug.Log (date);
+		Debug.Log (message);
+		Debug.Log (author);
+		Debug.Log (error);
+	}
+
+	private static string GetDRURL(string fetchDate, string lang) {
+		
+		string url = dr_web_service;
+		string delim = "?";
+		if (!fetchDate.Equals ("")) {
+			url = url + delim + "display_dt=" + fetchDate;
+			delim = "&";
+		}
+		url = url + delim + "lang=" + lang;
+		return url;
+	}
+
+	private static WWW StartDownload(string fetchDate, string lang) {
+		
+		Dictionary<string, string> headerInfo = new Dictionary<string, string> ();
+		WWW download = new WWW (GetDRURL (fetchDate, lang), null, headerInfo);
+		return download;
+	}
+
+	public static IEnumerator FetchDR(string fetchDate="", string lang="EN", Action<DailyReflection> callback=null) {
+		
+		WWW download = StartDownload (fetchDate, lang);
+		yield return download;
+		DailyReflection dr = new DailyReflection (lang, fetchDate, download.text, download.error);
+		dr.DebugLogDR ();
+		if (callback != null)
+			callback (dr);
 	}
 }
